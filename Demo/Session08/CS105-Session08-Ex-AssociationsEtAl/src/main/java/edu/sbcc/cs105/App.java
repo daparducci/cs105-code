76592e3f0660df6cb1e3342d03558033fa32a3dd
James Kinneavy <jskproxy@outlook.com>
2020-03-12 12:13:34 -0700

    Added associations example stub

---
+++ Demo/Session08/CS105-Session08-Ex-AssociationsEtAl/src/main/java/edu/sbcc/cs105/App.java
@@ -0,0 +1,13 @@
+package edu.sbcc.cs105;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
